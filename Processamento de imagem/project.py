789a1b1a1a0994fdc60a7766024a5bc1f8433291
leodefarias <lf25079@gmail.com>
2024-06-04 16:57:42 -0300

    Integração de Arduino finalizada

--- Processamento de imagem/project.py
+++ Processamento de imagem/project.py
@@ -1,27 +1,76 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
+import serial
+import serial.tools.list_ports
+import cv2
+from keras.models import load_model
 import numpy as np
-import cv2 
+import time
 
-model = load_model('Processamento de imagem\keras_model.h5')
-data = np.ndarray(shape=(1, 224, 224, 3), dtype = np.float32)
+# Inicialização do seriado e câmera
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+portList = []
+
+for one in ports:
+    portList.append(str(one))
+    print(str(one))
+
+com = input('Selecione a porta para o Arduino pelo número: ')
+found_port = False
+
+for i in range(len(portList)):
+    if portList[i].startswith('COM' + str(com)):
+        use = 'COM' + str(com)
+        print('Porta selecionada: ' + use)
+        found_port = True
+        break
+
+if not found_port:
+    print("Porta não encontrada!")
+    exit()
+
+serialInst.baudrate = 9600
+serialInst.port = use
+serialInst.open()
+
+# Carregar o modelo de aprendizado de máquina
+model = load_model('keras_model.h5')
+classes = ['fundo', 'controle', 'ao']
 
 cap = cv2.VideoCapture(0)
 
-classes = ['fundo', 'controle', 'mao']
-
 while True:
-    success,img = cap.read()
+    # Leitura da imagem da câmera
+    success, img = cap.read()
+    if not success:
+        print("Erro ao ler a imagem da câmera!")
+        break
+
     imgS = cv2.resize(img, (224, 224))
     image_array = np.asarray(imgS)
     normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     data[0] = normalized_image_array
-    prediction = model.predict(data)
-    print(prediction)
-    indexVal = np.argmax(prediction)
 
-    cv2.putText(img, str(classes[indexVal]),(50,50), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 2)
+    # Previsão do modelo
+    prediction = model.predict(data)
+    indexVal = np.argmax(prediction)
     print(classes[indexVal])
-    classe = classes[indexVal]
+
+    # Enviar comando ao Arduino
+    if classes[indexVal] == 'controle':
+        serialInst.write('ON'.encode('utf-8'))
+        print("Comando enviado: ON")
+    else:
+        serialInst.write('OFF'.encode('utf-8'))
+        print("Comando enviado: OFF")
+
+    # Exibir imagem com texto
+    cv2.putText(img, str(classes[indexVal]),(50,50), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 2)
     cv2.imshow('img',img)
-    cv2.waitKey(1)
-    
+
+    time.sleep(1)
+
+# Fechar a câmera e o seriado
+cap.release()
+cv2.destroyAllWindows()
+serialInst.close()
